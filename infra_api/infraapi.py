51c0d4fe81987c02896468b9a75ff31aeea23075
Ivar Grytten <ivar.grytten@gmail.com>
2017-04-07 10:49:31 +0000

    Debugging

--- infra_api/infraapi.py
+++ infra_api/infraapi.py
@@ -9,37 +9,39 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1 style='color:blue'>Hello There...........</h1>"
+    return "<h1 style='color:blue'>Hello There... asdf........</h1>"
 
 @app.route("/github_push", methods=['POST', 'GET'])
 
 def github_push():
 
     if request.method == "POST":
-	payload = json.loads(request.data)
-        repo_meta = {
-            'name': payload['repository']['name'],
-            'owner': payload['repository']['owner']['name'],
-        }
+		payload = json.loads(request.data)
+		repo_meta = {
+	        'name': payload['repository']['name'],
+	        'owner': payload['repository']['owner']['name'],
+	    }
 
-	app.logger.info("Recived post.")
+		app.logger.info("Recived post.")
 
-	clone_url = payload['repository']['clone_url']
-	app.logger.info("Clone url: %s" % clone_url)
-	handle_push_to_github(clone_url)
+		clone_url = payload['repository']['clone_url']
+		app.logger.info("Clone url: %s" % clone_url)
+		#handle_push_to_github(clone_url)
 
-	return str(request)
-
+		return str(request)
+		
     elif request.method == "GET":
-	f = open('log.txt', "w")
-	f.writelines(["Recived: " + str(request) + ""])
-	f.close()
-	return "Recieved get: " + str(request), 200
+		f = open('log.txt', "w")
+		f.writelines(["Recived: " + str(request) + ""])
+		f.close()
+		return "Recieved get : " + str(request), 200
 
     return "<h1 style='color:blue'>Test</h1>"
 
 if __name__ == "__main__":
 	handler = RotatingFileHandler('api.log', maxBytes=10000, backupCount=1)
-    	handler.setLevel(logging.INFO)
-    	app.logger.addHandler(handler)
-    	app.run(host='0.0.0.0')
+	handler.setLevel(logging.INFO)
+	app.logger.addHandler(handler)
+	app.run(host='0.0.0.0')
+
+   
