21d007a3da366e0dab66011afb754924de716686
Ivar Grytten <ivar.grytten@gmail.com>
2017-04-18 11:48:23 +0000

    Tests for installing package requirements

--- tests/testMethods.py
+++ tests/testMethods.py
@@ -2,12 +2,13 @@ import unittest
 from infra_api.methods import *
 import os
 import shutil
+from virtualenvapi.manage import VirtualEnvironment
 
 class testMethods(unittest.TestCase):
 	
 	def test_clone_github(self):
 		test_clone_url = 'https://github.com/ivargr/pythontest.git'
-		expected_clone_into_dir = "/home/github-repos/pythontest"		
+		expected_clone_into_dir = get_repo_dir("pythontest")		
 
 		if os.path.isdir(expected_clone_into_dir):
 			shutil.rmtree(expected_clone_into_dir)
@@ -28,3 +29,30 @@ class testMethods(unittest.TestCase):
 	def test_pull_github_repo(self):
 		pull_github_repo("/home/github-repos/pythontest")
 		self.assertTrue(True)
+		
+	def test_install_package_dependencies(self):
+		test_repo_name = "__testrepo__"
+		repo_dir = get_repo_dir(test_repo_name)
+		
+		if os.path.exists(repo_dir):
+			shutil.rmtree(repo_dir)
+		
+		os.makedirs(repo_dir)
+   			
+   		requirements = 	["virtualenv-api==2.1.16", "six"]
+   		requirements_file = open(repo_dir + "requirements.txt", "w")
+   		requirements_file.write('\n'.join(requirements))
+   		requirements_file.close()
+   		
+   		install_project_dependencies_into_virtual_env(test_repo_name)
+   		
+   		self.assertTrue(os.path.isdir(repo_dir + "virtual_env"))
+   		
+   		# Check that requirements are installed
+   		env = VirtualEnvironment(repo_dir + "virtual_env")
+   		for req in requirements:
+   			self.assertTrue(env.is_installed(req))
+   		
+ 
+if __name__ == "__main__":
+    unittest.main()
